e7d6dc10111b3b9596b2ddde435986f1d5a5bd54
HappyTomato0320 <dlatjden@naver.com>
2024-07-28 21:57:01 +0900

    AI_SpawnBox_Etc
    SpawnBox Actor와 Ai 연동 및 스폰 적용 기본 AI 구현 사망 구현
    충돌 구현(수정예정)
    Line Trace 수정

---
+++ Source/Base/Private/AIController_Dorongo.cpp
@@ -0,0 +1,58 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "AIController_Dorongo.h"
+#include "Kismet/GameplayStatics.h"
+#include "BehaviorTree/BlackboardComponent.h"
+#include "CPP_Dorongo.h"
+
+AAIController_Dorongo::AAIController_Dorongo()
+{
+	PrimaryActorTick.bCanEverTick = true;
+}
+
+void AAIController_Dorongo::BeginPlay()
+{
+    Super::BeginPlay();
+    
+    if(AIBehavior != nullptr)
+	{
+		RunBehaviorTree(AIBehavior);
+	}
+	// if (AIBehavior != nullptr)
+	// {
+	// 	RunBehaviorTree(AIBehavior);
+	
+	// 	APawn* PlayerPawn = UGameplayStatics::GetPlayerPawn(GetWorld(), 0);	
+        
+	// 	GetBlackboardComponent()->SetValueAsVector(TEXT("TargetLocation"), PlayerPawn->GetActorLocation());				
+	// 	GetBlackboardComponent()->SetValueAsVector(TEXT("StartLocation"), GetPawn()->GetActorLocation());        
+	// 	GetBlackboardComponent()->SetValueAsBool(TEXT("Attacking"), false); 		
+	// 	GetBlackboardComponent()->SetValueAsBool(TEXT("IsDead"), false);
+	
+	// }
+}
+
+void AAIController_Dorongo::Tick(float DeltaTime)
+{
+    Super::Tick(DeltaTime);
+
+    APawn* PlayerPawn = UGameplayStatics::GetPlayerPawn(GetWorld(), 0);	
+    
+	GetBlackboardComponent()->SetValueAsVector(TEXT("TargetLocation"), PlayerPawn->GetActorLocation());		
+	// // if (LineOfSightTo(PlayerPawn))
+	// // {
+	// // }
+	// // else
+	// // {
+	// // 	GetBlackboardComponent()->ClearValue(TEXT("TargetLocation"));
+	// // }
+
+	if (GetPawn()->GetDistanceTo(PlayerPawn) <= 800)
+	{
+		GetBlackboardComponent()->SetValueAsBool(TEXT("Attacking"), true);
+	}
+	else
+		GetBlackboardComponent()->SetValueAsBool(TEXT("Attacking"), false);
+}
+
---
+++ Source/Base/Private/CPP_Dorongo.cpp
@@ -0,0 +1,86 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "CPP_Dorongo.h"
+#include "DrawDebugHelpers.h"
+#include "Engine/Engine.h"
+
+// Sets default values
+ACPP_Dorongo::ACPP_Dorongo()
+{
+ 	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+	HP = MaxHP;
+}
+
+// Called when the game starts or when spawned
+void ACPP_Dorongo::BeginPlay()
+{
+	Super::BeginPlay();
+		
+}
+
+// Called every frame
+void ACPP_Dorongo::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+	 PerformLineTrace();
+
+}
+
+// Called to bind functionality to input
+void ACPP_Dorongo::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+	Super::SetupPlayerInputComponent(PlayerInputComponent);
+
+}
+
+void ACPP_Dorongo::PerformLineTrace()
+{
+	FHitResult HitResult;
+    FCollisionQueryParams Params;
+    Params.AddIgnoredActor(this); // 자기 자신을 무시
+
+    // 라인 트레이스를 수행할 지점 설정 Tick마다 위치 갱신
+    StartPoint = GetActorLocation();
+    EndPoint = StartPoint + FVector(GetActorForwardVector() * 400); // 앞으로 라인트레이스 생성
+
+    bool bHit = GetWorld()->LineTraceSingleByChannel(
+        HitResult,
+        StartPoint,
+        EndPoint,
+        ECC_Visibility,
+        Params
+    );
+
+    if (bHit)
+    {
+        GEngine->AddOnScreenDebugMessage(-1, 5.f, FColor::Red, FString::Printf(TEXT("Hit: %s"), *HitResult.GetActor()->GetName()));
+    }
+
+    // 디버그 라인 그리기
+    DrawDebugLine(
+        GetWorld(),
+        StartPoint,
+        EndPoint,
+        bHit ? FColor::Red : FColor::Green,
+        false,
+        1,
+        0,
+        1
+    );
+}
+
+void ACPP_Dorongo::TakeDamage(float DamageAmount)
+{
+    HP -= DamageAmount;
+
+    GEngine->AddOnScreenDebugMessage(-1, 5.f, FColor::Red, FString::Printf(TEXT("HP : %d"), HP));
+
+    if(HP <= 0.0f)
+    {
+        Destroy();
+    }
+}
