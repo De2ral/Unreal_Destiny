c94d9019bdba7a0df013e8dc5e4afb5f8d1313f3
iscream3115 <rhksgmlsp@gmail.com>
2024-07-29 13:46:58 +0900

    NonFPSCoreChar 클래스 삭제/ 오버랩 테스트용 무기 액터 추가

---
+++ Source/Base/Private/TestRifleActor.cpp
@@ -0,0 +1,31 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "TestRifleActor.h"
+
+// Sets default values
+ATestRifleActor::ATestRifleActor()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+}
+
+// Called when the game starts or when spawned
+void ATestRifleActor::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+
+// Called every frame
+void ATestRifleActor::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+	
+
+
+}
+
