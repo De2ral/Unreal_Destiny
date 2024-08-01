7def48d15a4713520128a103d7b3b78f6c15bb04
Luidingo <110881809+Luidingo@users.noreply.github.com>
2024-08-01 15:51:13 +0900

    스킬(방벽) 클래스 생성

---
+++ Source/Base/Private/Titan_Skill_Barrier.cpp
@@ -0,0 +1,27 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Titan_Skill_Barrier.h"
+
+// Sets default values
+ATitan_Skill_Barrier::ATitan_Skill_Barrier()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+}
+
+// Called when the game starts or when spawned
+void ATitan_Skill_Barrier::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void ATitan_Skill_Barrier::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
