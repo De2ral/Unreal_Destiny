ba7e8e2c5314dc0339fcede8597d3495e0e1c0f5
iscream3115 <rhksgmlsp@gmail.com>
2024-08-05 17:19:24 +0900

    아이템 컴포넌트 오류 수정 (C++)

--- Source/Base/Private/ItemComponent.cpp
+++ Source/Base/Private/ItemComponent.cpp
@@ -32,10 +32,10 @@ void UItemComponent::BeginPlay()
 void UItemComponent::OnOverlapBegin(UPrimitiveComponent *OverlappedComp, AActor *OtherActor, UPrimitiveComponent *OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult &SweepResult)
 {
 
-	if(OtherActor->ActorHasTag(TEXT("DestinyPlayer") && IsValid(Owner)))
-	{
-		Destory(Owner);
-	}
+	// if(OtherActor->ActorHasTag(TEXT("DestinyPlayer") && IsValid(Owner)))
+	// {
+	// 	Destory(Owner);
+	// }
 
 }
 
