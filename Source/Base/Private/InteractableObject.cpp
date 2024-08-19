b32bb8a35ddd969801c6dd8a13f69187ab5e414d
iscream3115 <rhksgmlsp@gmail.com>
2024-08-19 23:22:20 +0900

    새 브랜치 생성 후 안정화 / 상호작용 오브젝트 통합용 클래스 기반 생성 (C++)

---
+++ Source/Base/Private/InteractableObject.cpp
@@ -0,0 +1,35 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "InteractableObject.h"
+
+// Sets default values
+AInteractableObject::AInteractableObject()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+}
+
+// Called when the game starts or when spawned
+void AInteractableObject::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+void AInteractableObject::OnOverlapBegin(UPrimitiveComponent *OverlappedComp, AActor *OtherActor, UPrimitiveComponent *OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult &SweepResult)
+{
+}
+
+void AInteractableObject::OnOverlapEnd(UPrimitiveComponent *OverlappedComp, AActor *OtherActor, UPrimitiveComponent *OtherComp, int32 OtherBodyIndex)
+{
+}
+
+// Called every frame
+void AInteractableObject::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
---
+++ Source/Base/Public/InteractableObject.h
@@ -0,0 +1,38 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "InteractableObject.generated.h"
+
+UCLASS()
+class BASE_API AInteractableObject : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	// Sets default values for this actor's properties
+	AInteractableObject();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+	UPROPERTY(EditAnywhere)
+	class USphereComponent* ObjCollider;
+
+	UPROPERTY(EditAnywhere)
+	class UStaticMeshComponent* ObjMesh;
+
+	UFUNCTION()
+	void OnOverlapBegin(UPrimitiveComponent* OverlappedComp, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult);
+
+	UFUNCTION()
+	void OnOverlapEnd(UPrimitiveComponent* OverlappedComp, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex);
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+};
