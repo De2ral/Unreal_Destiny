5b6a5ae9b921b51e58cb37efe47a59c1c4d56cef
minsoo1254 <minsoo1254@naver.com>
2024-09-05 21:38:15 +0900

    커밋

--- Source/Base/Private/DestinyFPSBase.cpp
+++ Source/Base/Private/DestinyFPSBase.cpp
@@ -240,7 +240,7 @@ void ADestinyFPSBase::BeginPlay()
 			if (HUDWidget)
 			{
 				HUDWidget->AddToViewport();
-				HUDWidget->UpdateAmmo(WeaponComponent->CurrentAmmo(), WeaponComponent->StoredAmmo());
+				//HUDWidget->UpdateAmmo(WeaponComponent->CurrentAmmo(), WeaponComponent->StoredAmmo());
 				HUDWidget->UpdateSkillCoolTime(CurSkillCoolTime, SkillCoolTime);
 				HUDWidget->UpdateGrenadeCoolTime(CurGrenadeCoolTime, GrenadeCoolTime);
 				HUDWidget->UpdateMeleeCoolTime(CurMeleeAttackCoolTime, MeleeAttackCoolTime);
